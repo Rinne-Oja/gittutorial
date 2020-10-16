85e3c7298ffd5c69ca6e2bb6b9a12b87b9685e14
tkulojarvi <kulojarvi.teemu@gmail.com>
2020-10-16 14:56:23 +0300

    git commit -m mathtools

--- mathtools.py
+++ mathtools.py
@@ -1,4 +1,11 @@
-
+def fib(n):
+    ''' Calculates the n value of the fibonacci sequence'''
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
 
 def isPrime(n):
     '''Returns true if a number n is a prime number'''
